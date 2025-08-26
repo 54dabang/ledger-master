645ec9cc42cbbaa6c2db3fc5f18ed5feee03b74d
54dabang <leixingbang@163.com>
2025-08-26 14:53:50 +0800

    添加swgger注解

--- ledger-business/src/main/java/com/ledger/business/vo/SysUserVo.java
+++ ledger-business/src/main/java/com/ledger/business/vo/SysUserVo.java
@@ -1,15 +1,19 @@
 package com.ledger.business.vo;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class SysUserVo {
+    @ApiModelProperty(value = "用户id")
     private Long userId;
 
+    @ApiModelProperty(value = "登录用户名")
     private String userName;
 
+    @ApiModelProperty(value = "用户真实姓名")
     private String nickName;
 
 }
