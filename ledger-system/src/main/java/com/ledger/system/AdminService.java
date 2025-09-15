93bf2997e93dbb057c9ef1c473d7f9a98c794dc8
54dabang <leixingbang@163.com>
2025-09-15 17:14:25 +0800

    更改判断用户是否是管理员的逻辑

--- ledger-system/src/main/java/com/ledger/system/AdminService.java
+++ ledger-system/src/main/java/com/ledger/system/AdminService.java
@@ -12,7 +12,7 @@ public class AdminService {
     public static boolean isAdmin(Long userId) {
         ISysRoleService sysRoleService = ApplicationContextUtil.getBean(ISysRoleService.class);
         List<SysRole> roleList = sysRoleService.selectRolesByUserIdWithoutProxy(userId);
-        boolean isAdminRole = roleList.stream().map(r -> r.getRoleKey()).anyMatch(key -> key.equals("admin"));
+        boolean isAdminRole = roleList.stream().filter(r->r.isFlag()).map(r -> r.getRoleKey()).anyMatch(key -> key.equals("admin"));
         return SysUser.isAdmin(userId) || isAdminRole;
     }
 }
