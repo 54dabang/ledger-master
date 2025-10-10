a3fd96a5b979a53c5610de85dca0a37073ce5650
54dabang <leixingbang@163.com>
2025-10-10 14:49:21 +0800

    部门信息展示

--- ledger-business/src/main/java/com/ledger/business/vo/SysUserVo.java
+++ ledger-business/src/main/java/com/ledger/business/vo/SysUserVo.java
@@ -5,6 +5,8 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Optional;
+
 @Data
 @Builder
 public class SysUserVo {
@@ -17,10 +19,20 @@ public class SysUserVo {
     @ApiModelProperty(value = "用户真实姓名")
     private String nickName;
 
+    @ApiModelProperty(value = "性别")
+    private String sex;
+    @ApiModelProperty(value = "部门名称")
+    private String deptName;
+    @ApiModelProperty(value = "部门id")
+    private Long deptId;
+
     public static SysUserVo toSysUserVo(SysUser sysUser){
         return SysUserVo.builder().userId(sysUser.getUserId())
                 .userName(sysUser.getUserName())
                 .nickName(sysUser.getNickName())
+                .sex("0".equals(sysUser.getSex())?"男":"女")
+                .deptName(Optional.ofNullable(sysUser.getDept()).map(d->d.getDeptName()).orElse(""))
+                .deptId(Optional.ofNullable(sysUser.getDept()).map(d->d.getDeptId()).orElse(null))
                 .build();
     }
 
